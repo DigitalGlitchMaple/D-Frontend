3c1c63bbab51576301092f321f3cac68332c8a73
Gabriel <ioangabi97@gmail.com>
2023-02-25 13:39:09 +0200

    implemented log out funtionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from './Models/User';
+import { UserService } from './Services/user-service.service';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,23 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'FunReel';
   opened = false;
+  isUser: boolean;
+
+constructor( private userService: UserService){}
+
+ngOnInit(): void {
+  this.userService.user.subscribe(user => {
+    if(user.authdata){
+      this.isUser = true;
+    }
+    else{
+      this.isUser = false;
+    }
+  })
+}
+
+logout(){
+  this.userService.logout();
+}
+
 }
